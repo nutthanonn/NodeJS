7ae4a14413ea084a2a59825616b2bc62cff20cc1
nutthanon <nutthanon.tho@gmail.com>
2022-01-18 20:37:08 +0700

    :green_heart: index.ts

--- src/index.ts
+++ src/index.ts
@@ -3,6 +3,9 @@ import dotenv from "dotenv";
 import { createConnection } from "typeorm";
 import { Classroom } from "./entities/Classroom";
 import { Student } from "./entities/Student";
+import { Teacher } from "./entities/Teacher";
+import { createStudentRouter } from "./routes/create_student";
+import { createClassroomRouter } from "./routes/create_classroom";
 
 const app = express();
 dotenv.config();
@@ -16,16 +19,18 @@ const main = async () => {
       username: process.env.USERNAME,
       password: process.env.PASSWORD,
       database: "postgres",
-      entities: [Classroom, Student],
+      entities: [Classroom, Student, Teacher],
       synchronize: true,
     });
 
     app.use(express.json());
+    app.use(createStudentRouter);
+    app.use(createClassroomRouter);
 
+    console.log("------Connect Success------");
     app.listen(8000, () => {
       console.log("Running on port 8000");
     });
-    console.log("------Connect Success------");
   } catch (error) {
     console.log("--Connect Error--");
   }
